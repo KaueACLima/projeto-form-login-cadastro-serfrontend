e7f91ad2130f776f28d292588093b5dd93f5214b
Kauê Lima <kaue5939.lima@gmail.com>
2024-04-10 00:30:22 -0300

    Initial commit

---
+++ assets/js/desafio-aula344.js
@@ -0,0 +1,10 @@
+let primeiroNumero = prompt('Digite um número:')
+let segundoNumero = prompt('Digite mais um número:')
+
+primeiroNumero = parseInt(primeiroNumero);
+segundoNumero = parseInt(segundoNumero);
+
+alert("A divisão dos números digitados é: " + (primeiroNumero / segundoNumero));
+alert("A multiplicação dos números digitados é: " + (primeiroNumero * segundoNumero));
+alert("A subtração dos números digitados é: " + (primeiroNumero - segundoNumero));
+alert("A soma dos números digitados é: " + (primeiroNumero + segundoNumero));
